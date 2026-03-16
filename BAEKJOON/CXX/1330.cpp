1f78b06b42599f6db145fc3c487bd5fc49c1356d
JongSig <bnghty46@naver.com>
2026-03-16 22:24:41 +0900

    [백준] No.1330 두 수 비교하기

---
+++ BAEKJOON/CXX/1330.cpp
@@ -0,0 +1,31 @@
+/*
+    No.1330 두 수 비교하기
+
+    problem
+    두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램 작성
+
+    input
+    1 2
+
+    output
+    >
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int A, B;
+    
+    cin >> A >> B;
+    
+    if (A > B) {
+        cout << ">" << endl;
+    }else if (A < B) {
+        cout << "<" << endl;
+    } else {
+        cout << "==" << endl;
+    }
+
+    return 0;
+}
